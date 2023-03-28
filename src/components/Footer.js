770d8df2212f0191439677e10074ab4a9fd77594
Axel Paillaud <a.paillaud75@gmail.com>
2023-03-28 10:35:48 +0200

    my solution from the exercice P2C5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,13 @@ import { useState } from 'react'
 import '../style/Footer.css'
 
 function Footer() {
-	const [inputValue, setInputValue] = useState('')
+	const [inputValue, setInputValue] = useState('Entrez votre email ici');
+
+    function checkEmail(value) {
+        if (!value.includes("@")) {
+            alert("Erreur : Le champ ne contient pas le caractère '@', ce n'est pas une adresse mail valide");
+        }
+    }
 
 	return (
 		<footer className='lmj-footer'>
@@ -10,6 +16,9 @@ function Footer() {
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+            <form>
+                <input type="email" placeholder={ inputValue } onBlur={(e) => checkEmail(e.target.value) } />
+            </form>
 		</footer>
 	)
 }
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,20 +2,40 @@ import '../style/PlantItem.css'
 import sun from '../assets/sun.svg'
 import water from '../assets/water.svg'
 
-function handleClick(e) {
-    console.log(e);
-}
-
 function CareScale({scaleValue, careType}) {
     const range = [1, 2, 3];
 
     const scaleType = careType === 'light' ? sun : water;
 
+    function alertCareScale(e) {
+        let careTypeMessage;
+        let scaleValueMessage;
+
+        if (careType === 'light')
+            careTypeMessage = 'de lumière';
+        else
+            careTypeMessage = "d'arrosage";
+        
+        if (scaleValue === 1)
+            scaleValueMessage = 'peu';
+        else if (scaleValue === 2)
+            scaleValueMessage = 'modérement';
+        else
+            scaleValueMessage = 'beaucoup';
+
+        console.log(`Cette plante requiert ${scaleValueMessage} ${careTypeMessage}`);
+    }
+
     return (
         <div>
             {range.map(
                 (rangeElement) => scaleValue >= rangeElement ?
-                    (<img key={rangeElement.toString()} src={scaleType} alt='Care scale icon' />) : null
+                    (<img onClick={ alertCareScale } 
+                        key={rangeElement.toString()} 
+                        src={scaleType} 
+                        test="hello" 
+                        alt='Care scale icon' />)
+                         : null
             )}
         </div>
     )
@@ -23,7 +43,7 @@ function CareScale({scaleValue, careType}) {
 
 function PlantItem({name, cover, id, light, water}) {
     return (
-        <li className='lmj-plant-item' key={ id } onClick={ handleClick }>
+        <li className='lmj-plant-item' key={ id }>
             <img className='lmj-plant-item-cover' src={ cover } alt={ name } />
             { name }
             <CareScale careType='light' scaleValue={ light } />
